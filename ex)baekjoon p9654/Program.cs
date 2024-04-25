abe875e24494aa313568dd37058b8f1dafc88429
kimjinyoung <jdc8592@naver.com>
2024-04-25 21:33:11 +0900

    초기 커밋.

---
+++ ex)baekjoon p9654/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_baekjoon_p9654
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE"); // 개행을 하여야 하므로 writeline
+            Console.WriteLine("N2 Bomber      Heavy Fighter  Limited    21        "); // 1열, 2열: 15, 3열: 11, 4열: 10
+            Console.WriteLine("J-Type 327     Light Combat   Unlimited  1         ");
+            Console.WriteLine("NX Cruiser     Medium Fighter Limited    18        ");
+            Console.WriteLine("N1 Starfighter Medium Fighter Unlimited  25        ");
+            Console.WriteLine("Royal Cruiser  Light Combat   Limited    4         ");
+        }
+    }
+}
